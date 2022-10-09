04b73b53115bb732305e7e15d9ad01ac065d0afa
Lokeshraj-m <finzly_lokesh@outlook.com>
2022-10-09 20:18:28 +0530

    Updated crossOrgin

--- FXTrading/src/main/java/com/fxTrading/controller/FXTradingController.java
+++ FXTrading/src/main/java/com/fxTrading/controller/FXTradingController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,8 @@ import com.fxTrading.entity.User;
 import com.fxTrading.service.FXTradingServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/FxTrading")
 public class FXTradingController {
 
 	@Autowired
